ee003f7c753f804f3d4063d03c684221f86728ab
Andy Stoica <andy.stoica@me.com>
2017-02-27 18:03:49 +0000

    Rearranged directory structure

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var Book = require('../models').Book;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+  Book.findAll().then((books) => {console.log(books)});
   res.end();
   console.log('GET / Home page');
 });
