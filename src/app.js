9687004b313a016b682c7478f001dea309791966
Andy Stoica <andy.stoica@me.com>
2017-03-01 17:56:14 +0000

    Code cleanup

--- src/app.js
+++ src/app.js
@@ -1,39 +1,55 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
+'use strict';
+var express    = require('express');
+var path       = require('path');
+var favicon    = require('serve-favicon');
+var logger     = require('morgan');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var books = require('./routes/books');
-var patrons = require('./routes/patrons');
-var loans = require('./routes/loans');
+// Route handlers
+var index      = require('./routes/index');
+var books      = require('./routes/books');
+var patrons    = require('./routes/patrons');
+var loans      = require('./routes/loans');
 
-var app = express();
+var app        = express();
 
-// view engine setup
+
+
+/**
+ * View engine setup
+ */
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
-
 app.use(favicon(path.join(__dirname, 'public', 'images/favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+/**
+ * Assign route handlers
+ */
 app.use('/', index);
 app.use('/books', books);
 app.use('/patrons', patrons);
 app.use('/loans', loans);
 
-// catch 404 and forward to error handler
+
+
+/**
+ * Error handling
+ */
+
+// Catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Oh dear, this page is missing...');
   err.status = 404;
   next(err);
 });
 
-// error handler
+// Error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
@@ -43,5 +59,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
